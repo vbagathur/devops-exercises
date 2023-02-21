9544de9dea5551df93db74fb6a490798aac75293
Contractor Viswanath Natarajan <vnatarajan@llbean.com>
2023-02-20 22:36:03 -0500

    add javadoc

--- week5-chapter8/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ week5-chapter8/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+	Calculator to calculate sum of two numbers
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
